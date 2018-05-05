280fd456ffd5cdc05c8cf3a57f26e6f645117e31
Itamar Abeliovich <iabeliovich@gmail.com>
2018-05-05 16:33:03 +0300

    Basic FRP

--- src/frp.hs
+++ src/frp.hs
@@ -1,6 +1,25 @@
+module FRP where
+
 import Reactive.Banana
 import Reactive.Banana.Frameworks.AddHandler
 import Reactive.Banana.Frameworks
 import Control.Monad (forever)
+import Control.Monad.State (execState)
+import Mechanism
 
+makeNetworkDescription :: (Frameworks t) => AddHandler Char -> Moment t ()
+makeNetworkDescription addKeyEvent =  do
+    eKey <- fromAddHandler addKeyEvent
+    let 
+        eInsertChar = execInsert <$> eKey
+        bState = accumB initialState eInsertChar
+    eStateChanged <- changes bState
+    reactimate' $ fmap (\state -> putStrLn ("Text: " ++ getText state ++ "; Pointer: " ++ getPointer state))
+                  <$> eStateChanged
 
+main :: IO ()
+main = do
+    (addKeyEvent, fireEvent) <- newAddHandler
+    network <- compile $ makeNetworkDescription addKeyEvent
+    actuate network
+    forever $ getChar >>= fireEvent
